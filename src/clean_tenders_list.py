23879cdee7ed940c0f79242ef14cb1a7c63b1948
Kuki Amarilla <sc.amarilla@gmail.com>
2025-03-02 17:01:16 -0300

    change output preprocess file to csv

--- src/clean_tenders_list.py
+++ src/clean_tenders_list.py
@@ -4,7 +4,7 @@ import pandas as pd
 def preprocess(input_path, output_path):
     df = pd.read_excel(input_path)
     df_filtered = df[pd.to_numeric(df['ID licitación'], errors='coerce').notna()]
-    df_filtered.to_excel(output_path, index=False)
+    df_filtered.to_csv(output_path, index=False)
 
 if __name__ == "__main__":
     input_path = sys.argv[1]
